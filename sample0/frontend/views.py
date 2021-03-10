9832defb1cbc4468cb4bf5e97fd54f2c1f8e88bc
Grandayyyy <grandy.maddhav18@st.niituniversity.in>
2021-03-10 13:18:44 +0530

    changes made

--- sample0/frontend/views.py
+++ sample0/frontend/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render (request, 'index.html')
+
+def about(request):
+    return render (request, 'about.html')
+
+def industries(request):
+    return render (request, 'industries.html')
+
+def contact(request):
+    return render (request, 'contact.html')
+    
