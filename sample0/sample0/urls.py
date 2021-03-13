39cc2383c6d0742c66b899d6217cdd398a58b607
pdeepak350 <patel.polaram18@st.niituniversity.in>
2021-03-13 17:22:30 +0530

    Removed Botmain

--- sample0/sample0/urls.py
+++ sample0/sample0/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('botmain/', include('botmain.urls')),
 ]
 
 
