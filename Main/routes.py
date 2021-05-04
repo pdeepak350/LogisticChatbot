6ede76b6339a2fd3f12e9eba596aee6550793489
trisha-151 <trisha.sharma18@st.niituniversity.in>
2021-05-04 16:45:58 +0530

    add cart

--- Main/routes.py
+++ Main/routes.py
@@ -666,14 +666,14 @@ def results():
     elif queryResult['action']== "item.add":       
         try:
             quantity = queryResult['parameters']['quantity']
+            return {'fulfillment' : quantity}
             email =  queryResult['parameters']['email']
             user = user.query.filter_by(email = email).first()
             user_id = user.id
             product = queryResult['parameters']['product']
             category_id = Category.query.filter_by(name = product).first()
             product = Addproduct.query.filter_by(category_id=category_id.id).first()
-            cart = Cart.query.filter_by(user_id=user_id, product_id=product.id).first() 
-            return {'fulfillment' : "1"}
+            cart = Cart.query.filter_by(user_id=user_id, product_id=product.id).first()
             if cart is None and quantity <= product.stock:
                 addcart = Cart(user_id=user_id, product_id=product.id, quantity=quantity)
                 db.session.add(addcart)
