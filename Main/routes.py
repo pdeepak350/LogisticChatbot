d7aa7ba0666393ca23cc7487386f11973baca81e
Deepak Patel <patel.polaram18@st.niituniversity.in>
2021-05-05 09:57:40 +0530

    Fixes

--- Main/routes.py
+++ Main/routes.py
@@ -678,7 +678,7 @@ def results():
             category_id = Category.query.filter_by(name=product).first()
             products = Addproduct.query.filter_by(category_id=category_id).first()
             cart = Cart.query.filter_by(user_id=user_id, product_id=products.id).first()
-            return {'fulfillmentText': cart}
+            return {'fulfillment': product + quantity + email + products}
             if cart is None and quantity <= products.stock:
                 addcart = Cart(user_id=user_id, product_id=products.id, quantity=quantity)
                 db.session.add(addcart)
