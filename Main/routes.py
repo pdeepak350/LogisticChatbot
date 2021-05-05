cf1fb563786d093b79e6ea1d3c06922edda56a23
Deepak Patel <patel.polaram18@st.niituniversity.in>
2021-05-05 08:27:30 +0530

    Fixes

--- Main/routes.py
+++ Main/routes.py
@@ -657,9 +657,8 @@ def results():
         product = queryResult['parameters']['product']
         category_id = Category.query.filter_by(name=product).first()
         product_id = Addproduct.query.filter_by(category_id=category_id.id).first()
-        #return {'fulfillmentText': "Before Prod"}
         prod = url_for('product',id=product_id.id)
-        return {'fulfillmentText': 'Here is your searched item: '+prod}
+        return {'fulfillmentText': 'Here is your searched item: '+'https://plox/.dev'+prod}
     # elif queryResult['action'] == "cart_check":
     #     return {'fulfillmentText': "visit "+value+" to check items you have added"}
     # elif queryResult['action'] == "check_out":   
